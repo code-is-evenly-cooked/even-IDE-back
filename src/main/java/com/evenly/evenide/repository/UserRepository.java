6db875fe5e72ee4034c1b421c09d278885ad7575
LJIEUN <eun773c@daum.net>
2025-04-09 01:50:47 +0900

    feat: 회원가입 Repository 추가

---
+++ src/main/java/com/evenly/evenide/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.evenly.evenide.repository;
+
+import com.evenly.evenide.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일 중복 체크
+    boolean existsByEmail(String email);
+
+    // 닉네임 중복 체크
+    boolean existsByNickname(String nickname);
+
+    // 사용할 이메일 기준 조회
+    Optional<User> findByEmail(String email);
+}
