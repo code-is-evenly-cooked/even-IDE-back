67ba0458afc7bb4dc769a7e4d46e996d51409aa5
LJIEUN <eun773c@daum.net>
2025-04-09 03:52:25 +0900

    ref: 유효성 검사 실패시 발생하는 예외 처리

--- src/main/java/com/evenly/evenide/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/evenly/evenide/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.evenly.evenide.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +14,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String error = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
     }
 }
