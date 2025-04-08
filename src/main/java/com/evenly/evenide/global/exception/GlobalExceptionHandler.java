d950bcd57c6ee0a762f1ba502c7dc0484671b9f8
LJIEUN <eun773c@daum.net>
2025-04-09 03:16:41 +0900

    feat: 전역 처리 클래스 추가 (CustomException)

---
+++ src/main/java/com/evenly/evenide/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.evenly.evenide.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException e) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+}
