047c40e6520bbf3c7d125acd5e3dc42a598a8f81
LJIEUN <eun773c@daum.net>
2025-04-10 20:41:53 +0900

    ref: ErrorCode 추가 (로그인)

--- src/main/java/com/evenly/evenide/global/exception/ErrorCode.java
+++ src/main/java/com/evenly/evenide/global/exception/ErrorCode.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
-    NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다.");
+    NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "가입되지 않은 사용자 입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀립니다.");
+
 
     private final HttpStatus httpStatus;
     private String message;
