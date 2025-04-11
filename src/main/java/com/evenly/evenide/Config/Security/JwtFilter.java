9fe681525ac841524627f6b686382a3646c8b03a
LJIEUN <eun773c@daum.net>
2025-04-11 19:37:20 +0900

    ref: 1차 filter 및 config 수정

--- src/main/java/com/evenly/evenide/Config/Security/JwtFilter.java
+++ src/main/java/com/evenly/evenide/Config/Security/JwtFilter.java
@@ -7,12 +7,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
@@ -20,6 +20,12 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException{
+
+        String path = request.getRequestURI();
+        if (path.startsWith("/auth/") || path.startsWith("/mock/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         // Authorization Header 에서 토큰 꺼내는 부분
         String token = resolveTokenFromHeader(request);
--- src/main/java/com/evenly/evenide/Config/Security/SecurityConfig.java
+++ src/main/java/com/evenly/evenide/Config/Security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,7 +20,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-
+    private final JwtUtil jwtUtil;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -28,9 +29,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/mock/**").permitAll() // mock api
-                        .requestMatchers("/auth/login", "/auth/signup").permitAll() // 로그인, 회원가입 인증 없이
+                        .requestMatchers("/auth/**").permitAll() // 로그인, 회원가입 인증 없이
                         .anyRequest().authenticated() // 그외 모든 요청은 인증 필요
-                );
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
