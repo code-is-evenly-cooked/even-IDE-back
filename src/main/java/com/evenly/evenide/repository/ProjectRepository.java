b9f51eaf766490ad46ee7fa4aecbcda4efdb95d7
nahyun <knahyuk@gmail.com>
2025-04-11 19:43:39 +0900

    feat: 코드파일, 프로젝트 레포지토리 추가

--- src/main/java/com/evenly/evenide/repository/ProjectRepository.java
+++ src/main/java/com/evenly/evenide/repository/ProjectRepository.java
@@ -5,7 +5,9 @@ import com.evenly.evenide.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findAllByOwner(User owner);
+    Optional<Project> findByShareToken(String token);
 }
