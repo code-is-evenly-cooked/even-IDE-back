1f96dd7a903f8d01433eb49b7abffc34582b1497
LJIEUN <eun773c@daum.net>
2025-04-11 18:29:46 +0900

    feat: JwtFilter 추가

---
+++ src/main/java/com/evenly/evenide/Config/Security/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.evenly.evenide.Config.Security;
+
+import com.evenly.evenide.dto.JwtUserInfoDto;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException{
+
+        // Authorization Header 에서 토큰 꺼내는 부분
+        String token = resolveTokenFromHeader(request);
+
+        if (token != null && jwtUtil.validateAccessToken(token)) {
+            try {
+                String userId = jwtUtil.getUserIdFromToken(token);
+                JwtUserInfoDto userInfoDto = new JwtUserInfoDto(userId);
+
+                // 인증 객체 생성 하는 부분
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userInfoDto, null, null);
+
+                // SecurityContext에 등록
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JwtException e) {
+                logger.warn("Invalid JWT: " + e.getMessage());
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveTokenFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
