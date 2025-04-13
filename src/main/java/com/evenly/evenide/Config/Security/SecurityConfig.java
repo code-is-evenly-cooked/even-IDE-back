1e7b23e42416a02b3d09aa63ee4867f0f5adf107
LJIEUN <eun773c@daum.net>
2025-04-13 17:35:11 +0900

    fix: import 주석제거

--- src/main/java/com/evenly/evenide/Config/Security/SecurityConfig.java
+++ src/main/java/com/evenly/evenide/Config/Security/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.evenly.evenide.Config.Security;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
