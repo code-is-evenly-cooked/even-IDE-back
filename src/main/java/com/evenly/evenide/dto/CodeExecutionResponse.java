cc91bb61311ea37b4ba83bb6cc52e89dba36eb8c
nahyun <knahyuk@gmail.com>
2025-04-15 02:57:52 +0900

    feat: 코드 실행 응답 dto

---
+++ src/main/java/com/evenly/evenide/dto/CodeExecutionResponse.java
@@ -0,0 +1,12 @@
+package com.evenly.evenide.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeExecutionResponse {
+    private String result;
+}
