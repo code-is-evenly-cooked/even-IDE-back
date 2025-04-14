709216f6214c12b430ecb32ea84b5d7d1b4cabc7
nahyun <knahyuk@gmail.com>
2025-04-14 11:18:56 +0900

    fix: 프로젝트 id 랜덤 숫자로 생성되는 버그 수정

--- src/main/java/com/evenly/evenide/entity/Project.java
+++ src/main/java/com/evenly/evenide/entity/Project.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Project {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
