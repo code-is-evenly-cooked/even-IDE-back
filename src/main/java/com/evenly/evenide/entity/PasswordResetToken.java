dbfffdc0a001f909965ff37a6cb8ceebb698d874
LJIEUN <eun773c@daum.net>
2025-04-16 01:18:55 +0900

    feat: 비밀번호 재설정 토큰 엔티티 추가

---
+++ src/main/java/com/evenly/evenide/entity/PasswordResetToken.java
@@ -0,0 +1,31 @@
+package com.evenly.evenide.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiration;
+
+    @OneToOne
+    @JoinColumn(name = "user_Id", nullable = false, unique = true)
+    private User user;
+}
