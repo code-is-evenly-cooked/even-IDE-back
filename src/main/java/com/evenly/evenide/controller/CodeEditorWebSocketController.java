0ef7a4055bd4eeabb2b1923738d290de62dbe6b8
nahyun <knahyuk@gmail.com>
2025-04-17 22:08:55 +0900

    feat: 전체 코드 업데이트 및 redis 저장 컨트롤러

---
+++ src/main/java/com/evenly/evenide/controller/CodeEditorWebSocketController.java
@@ -0,0 +1,36 @@
+package com.evenly.evenide.controller;
+
+
+import com.evenly.evenide.dto.CodeDiffMessage;
+import com.evenly.evenide.dto.CodeUpdateMessage;
+import com.evenly.evenide.service.CodeSyncService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class CodeEditorWebSocketController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final CodeSyncService codeSyncService;
+
+    @MessageMapping("/code.update")
+    public void handleCodeUpdate(CodeUpdateMessage message) {
+        if (message.getTimestamp() == null) {
+            message.setTimestamp(LocalDateTime.now());
+        }
+
+        codeSyncService.saveCodeUpdateLog(message);
+
+        messagingTemplate.convertAndSend(
+                "/topic/project/" + message.getProjectId() + "/file/" + message.getFileId(),
+                message
+        );
+    }
+}
