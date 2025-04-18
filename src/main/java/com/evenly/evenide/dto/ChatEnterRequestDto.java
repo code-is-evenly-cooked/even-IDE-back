d23425ceefa6f2c26ffa2b59c797cba18a66d2b0
nahyun <knahyuk@gmail.com>
2025-04-18 12:50:02 +0900

    fix: 채팅방(프로젝트) 진입시 fileId 요청

--- src/main/java/com/evenly/evenide/dto/ChatEnterRequestDto.java
+++ src/main/java/com/evenly/evenide/dto/ChatEnterRequestDto.java
@@ -1,8 +1,14 @@
 package com.evenly.evenide.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ChatEnterRequestDto {
+
+    @NotNull(message = "projectId는 필수입니다.")
     Long projectId;
+
+    @NotNull(message = "fileId는 필수입니다.")
+    Long fileId;
 }
