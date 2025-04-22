f72e768e0c839cade5816fcb0a5b79c214364cf7
LJIEUN <eun773c@daum.net>
2025-04-23 00:35:22 +0900

    fix: 메모기능 관련 repository 수정

--- src/main/java/com/evenly/evenide/repository/CodeFileRepository.java
+++ src/main/java/com/evenly/evenide/repository/CodeFileRepository.java
@@ -3,5 +3,8 @@ package com.evenly.evenide.repository;
 import com.evenly.evenide.entity.CodeFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CodeFileRepository extends JpaRepository<CodeFile, Long> {
+    List<CodeFile> findAllByProjectId(Long projectId);
 }
