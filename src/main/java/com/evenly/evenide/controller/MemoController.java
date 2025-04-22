103f87b945378e3c3bda7f48ad315fa47bac8d0f
LJIEUN <eun773c@daum.net>
2025-04-23 00:36:14 +0900

    ref: 메모기능 관련 컨트롤러 수정

--- src/main/java/com/evenly/evenide/controller/MemoController.java
+++ src/main/java/com/evenly/evenide/controller/MemoController.java
@@ -3,10 +3,13 @@ package com.evenly.evenide.controller;
 import com.evenly.evenide.dto.*;
 import com.evenly.evenide.global.response.MessageResponse;
 import com.evenly.evenide.service.MemoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,31 +20,48 @@ public class MemoController {
 
     // 메모 생성
     @PostMapping
-    public ResponseEntity<MemoCreateResponse> createMemo(@RequestBody MemoCreateRequest request, @RequestParam int lineNumber,
-                                                      @AuthenticationPrincipal JwtUserInfoDto userInfo) {
-        Long memoId = memoService.createMemo(request, Long.parseLong(userInfo.getUserId()), lineNumber);
-        return ResponseEntity.ok(
-                new MemoCreateResponse(memoId,"success")
+    public ResponseEntity<MemoCreateResponse> createMemo(@RequestBody @Valid MemoCreateRequest request,
+                                                         @AuthenticationPrincipal JwtUserInfoDto userInfo) {
+        MemoCreateResponse response = memoService.createMemo(
+                request,
+                Long.parseLong(userInfo.getUserId())
         );
+        return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/lookup")
-    public ResponseEntity<MemoLookupResponse> lookupMemo(@RequestBody MemoLookupRequest request) {
-        return ResponseEntity.ok(memoService.lookup(request));
+
+    // 메모 전체 조회
+    @GetMapping
+    public ResponseEntity<List<MemoResponse>> getMemos(@RequestParam Long projectId) {
+        List<MemoResponse> responses = memoService.getAllMemos(projectId);
+        return ResponseEntity.ok(responses);
     }
+
+
+    // 메모 단일 조회
+    @GetMapping("/{memoId}")
+    public ResponseEntity<MemoSimpleResponse> getMemo(@PathVariable Long memoId) {
+        MemoSimpleResponse response = memoService.getMemo(memoId);
+        return ResponseEntity.ok(response);
+    }
+
 
     // 메모 수정
-    @PatchMapping("/{id}")
-    public ResponseEntity<MessageResponse> updateMemo(@PathVariable Long id, @AuthenticationPrincipal JwtUserInfoDto userInfo,
-                                           @RequestBody MemoUpdateRequest request) {
-        memoService.updateMemo(id, userInfo.getUserId(), request);
-        return ResponseEntity.ok(new MessageResponse("success"));
+    @PatchMapping
+    public ResponseEntity<MemoSimpleResponse> updateMemo(@RequestBody MemoUpdateRequest request,
+                                                   @AuthenticationPrincipal JwtUserInfoDto userInfo) {
+        MemoSimpleResponse response = memoService.updateMemo(
+                request.getMemoId(),
+                request,
+                Long.parseLong(userInfo.getUserId()));
+        return ResponseEntity.ok(response);
     }
 
     // 메모 삭제
-    @DeleteMapping("/{id}")
-    public ResponseEntity<MessageResponse> deleteMemo(@PathVariable Long id, @AuthenticationPrincipal JwtUserInfoDto userInfo) {
-        memoService.deleteMemo(id, userInfo.getUserId());
+    @DeleteMapping("/{memoId}")
+    public ResponseEntity<MessageResponse> deleteMemo(@PathVariable Long memoId, @AuthenticationPrincipal JwtUserInfoDto userInfo) {
+        memoService.deleteMemo(memoId, Long.parseLong(userInfo.getUserId()));
         return ResponseEntity.ok(new MessageResponse("success"));
     }
+
 }
